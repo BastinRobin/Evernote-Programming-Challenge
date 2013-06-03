4e04d277e5258a86df73eeeb5105a2de14b4e9df
Bastin Robin <bastinrobins@gmail.com>
2013-06-03 08:00:43 +0530

    Update MultiplyExceptSelf.py
    Added The input modules for testcase

--- MultiplyExceptSelf.py
+++ MultiplyExceptSelf.py
@@ -1,6 +1,19 @@
 #!/bin/python
+#Multiplication of list elements
 def mul(args):
     x= 1
     for arg in args:
        x*= arg
     return x
+    
+#Get the number of testcase as input
+n = input()
+l = []
+#Get element for each test case
+for i in range(n):
+    l.append(input())
+#Perform the final operation
+for ind, j in enumerate(l):
+	tmp = l.pop(ind)
+	print mul(l)
+	l.insert(ind, tmp)
